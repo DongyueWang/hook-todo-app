030f3435ee0146b0ad6c2aa2d4e3f355bb76c604
dongyue <dongyue.wang@external.loccitane.com>
2021-01-06 14:44:05 +0100

    function, hook commit

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,4 +1,36 @@
-import React from "react";
+import React, { useEffect } from "react";
+
+const TodoItem = (props) => {
+  const completedStyle = {
+    fontStyle: "italic",
+    color: "#d35e0f",
+    opacity: 0.4,
+    textDecoration: "line-through",
+  };
+
+  const { completed, id, title } = props.todo;
+
+  useEffect(() => {
+    return () => {
+      alert("Item about to be deleted!");
+    };
+  }, []);
+
+  return (
+    <li className="todo-item">
+      <input
+        type="checkbox"
+        checked={completed}
+        onChange={() => props.handleChangeProps(id)}
+      />
+      <button onClick={() => props.deleteTodoProps(id)}>Delete</button>
+      <span style={completed ? completedStyle : null}>{title}</span>
+    </li>
+  );
+};
+
+export default TodoItem;
+/*import React from "react";
 
 class TodoItem extends React.Component {
   componentWillUnmount() {
@@ -29,4 +61,4 @@ class TodoItem extends React.Component {
   }
 }
 
-export default TodoItem;
+export default TodoItem;*/
--- src/components/TodosList.js
+++ src/components/TodosList.js
@@ -1,6 +1,25 @@
 import React from "react";
 import TodoItem from "./TodoItem";
 
+const TodosList = (props) => {
+  return (
+    <div>
+      {props.todos.map((todo) => (
+        <TodoItem
+          key={todo.id}
+          todo={todo}
+          handleChangeProps={props.handleChangeProps}
+          deleteTodoProps={props.deleteTodoProps}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default TodosList;
+/*import React from "react";
+import TodoItem from "./TodoItem";
+
 class TodosList extends React.Component {
   render() {
     return (
@@ -18,4 +37,4 @@ class TodosList extends React.Component {
   }
 }
 
-export default TodosList;
+export default TodosList;*/
