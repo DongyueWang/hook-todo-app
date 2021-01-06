c8d6503e1bf5254c8a6682653675ba983d5578da
dongyue <dongyue.wang@external.loccitane.com>
2021-01-06 15:02:29 +0100

    Updates

---
+++ precache-manifest.7212f452880a0bc1502d88abe274fba0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45eeff3c348c97d5231026fea084774a",
+    "url": "/hook-todo-app/index.html"
+  },
+  {
+    "revision": "0adf3f98cb679c23bd26",
+    "url": "/hook-todo-app/static/css/main.b1e8f748.chunk.css"
+  },
+  {
+    "revision": "e7563aaee5e8f22c2506",
+    "url": "/hook-todo-app/static/js/2.4b470c50.chunk.js"
+  },
+  {
+    "revision": "0adf3f98cb679c23bd26",
+    "url": "/hook-todo-app/static/js/main.415b3aed.chunk.js"
+  },
+  {
+    "revision": "d0fbd885e0a81b7bda82",
+    "url": "/hook-todo-app/static/js/runtime-main.39034d9e.js"
+  }
+]);
